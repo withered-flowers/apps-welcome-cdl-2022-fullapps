a5c63e957ffefafbcad3e2aef0aa421d2fb496d1
Withered Flowers <withered-flowers@users.noreply.github.com>
2022-03-15 21:17:50 +0700

    fix: change event end time

--- src/lib/configs/config.ts
+++ src/lib/configs/config.ts
@@ -1,5 +1,5 @@
 export const eventTimeStart: string = "2022-03-07T00:00:00.000+07:00";
-export const eventTimeEnd: string = "2022-03-15T23:59:59.000+07:00";
+export const eventTimeEnd: string = "2022-03-16T23:59:59.000+07:00";
 
 export const LIST_OF_QUESTS: string[] = [
   "Introduction to Digital Transformation with Google Cloud",
